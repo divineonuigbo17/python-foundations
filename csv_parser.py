c2f61b3615282259fcce3bfbe91f12c93b03c00c
Divine Onuigbo <divineonuigbo17@gmail.com>
2026-02-21 19:52:54 +0000

    Add CSV parser mini-project

---
+++ csv_parser.py
@@ -0,0 +1,19 @@
+import csv
+
+# Open the CSV file
+with open('data.csv', 'r') as file:
+    reader = csv.DictReader(file)
+
+    total_Score = 0
+    row_count = 0
+
+    for row in reader:
+        print(f"{row['Name']} scored {row['Score']}")
+        total_Score += int(row['Score'])
+        row_count +=1
+
+# Calculate average
+if row_count > 0:
+    average = total_Score / row_count
+    print(f"Total rows: {row_count}")
+    print(f"Average score: {average:.2f}")
